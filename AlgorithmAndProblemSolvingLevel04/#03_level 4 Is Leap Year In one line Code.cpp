db91a9667c3a1c70aa3fa9cffd949c16702eeaba
ELKHATTAB SMAIL <elhattab207@gmail.com>
2025-02-26 19:15:33 +0000

    Add a Problem.

---
+++ AlgorithmAndProblemSolvingLevel04/#03_level 4 Is Leap Year In one line Code.cpp
@@ -0,0 +1,25 @@
+
+#include <iostream>
+using namespace std;
+
+short ReadYear()
+{
+    short Y = 0;
+    cout << "Enter The Year For Check If a Leap Year Or Not ? \n";
+    cin >> Y;
+    return Y;
+}
+
+bool IsLeapYear(short Y)
+{
+    return Y % 400 == 0 || (Y % 4 == 0 && Y % 100 != 0);
+}
+int main()
+{
+    short Year = ReadYear();
+    if (IsLeapYear(Year))
+        cout << Year << " Yes Its a Leap Year.\n";
+    else
+        cout << Year << " Its Not a Leap Year.\n";
+    system("\npause");
+}
